df9868f795b79401f643fd553c3985a86a78fb50
sumitchougule <sumitchougule80@gmail.com>
2018-04-14 12:20:17 +0000

    new feature

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,11 +3,11 @@ app.controller('chatController', function ($scope) {
     $scope.messages = [{
             'sender': 'USER',
             'text': 'hello'
- },
+		},
         {
             'sender': 'BOT',
             'text': 'hii what can i do for u?'
- }
+ 		}
  ];
 
 });
