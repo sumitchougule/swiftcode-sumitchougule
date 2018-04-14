6a0849fda2f0af62a3d64ccf0447307d5d8e5965
sumitchougule <sumitchougule80@gmail.com>
2018-04-14 11:53:23 +0000

    activator cmt

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,13 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [{
+            'sender': 'USER',
+            'text': 'hello'
+ },
+        {
+            'sender': 'BOT',
+            'text': 'hii what can i do for u?'
+ }
+ ];
 
 });
